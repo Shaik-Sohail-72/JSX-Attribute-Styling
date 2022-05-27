89128d2728ad5cf35afeb6df8a7dc36dadcacbc0
sohail <shaiksohailhu7n@gmail.com>
2022-05-27 20:06:02 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+const img="https://picsum.photos/200/300/?blur";
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img src={img}></img>
+    <div>
+    <img className="food-img" src="https://media.istockphoto.com/photos/fish-biryani-with-basmati-rice-indian-food-picture-id488481490?k=20&m=488481490&s=612x612&w=0&h=HYP2KxiC1e2tAtzmfrA7xxs3u8LD1wjSLPUD9bZ48eU="></img>
+    <img className="food-img" src="https://www.cookwithmanali.com/wp-content/uploads/2021/08/Schezwan-Noodles.jpg"></img>
+    <img className="food-img" src="https://static.toiimg.com/thumb/54504752.cms?width=1200&height=900"></img>
+    </div>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
